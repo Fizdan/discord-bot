fd1c464801854cb462b3577ec0f1bda39cfcff1d
Fizdan <hafizhfauzan24@gmail.com>
2022-03-28 06:27:29 +0700

    First Init

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+const { Client, Intents } = require("discord.js");
+const bodyParser = require("body-parser");
+const port = 80;
+const app = express();
+
+app.use(bodyParser());
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+client.login(process.env.tokenID);
+
+app.post("/", async (req, res) => {
+  const channel = client.channels.cache.get(process.env.channelID);
+  channel.send({ content: req.body.message });
+  res.send("Message Sent");
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
